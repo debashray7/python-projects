d9e20468b3285cc5ce253bad74d207fdf2ce5b89
debashray7 <debashraymohanty@gmail.com>
2025-10-12 13:34:53 +0530

    Fix bugs and errors in temperature_converter.py
    Give a menu to the user, User selects operation, Handle ValueError, Handle bugs if temperature is below 0K / -273.15C / -459.67F, Print "It's freezing!" if temperature is below 273.15K / 0C / 32F, Return to menu (until user chooses Exit)

--- chapter1-basics/temperature_converter.py
+++ chapter1-basics/temperature_converter.py
@@ -1,13 +1,52 @@
-temp = float(input('Enter temperature in Celsius: '))
-print(f'''
-Temperature Conversions:
-{temp}°C = {(temp*9/5)+32}°F
-{temp}°C = {temp+273.15}K
-''')
-if temp < 0:
-    print('''It\'s freezing!
-          ''')
-temp = float(input('Enter temperature in Fahrenheit: '))
-print(f'''
-Temperature Conversions:
-{temp}°F = {round(5*(temp-32)/9, 2)}°C''')
+while True:
+    print('''===== TEMPERATURE CONVERSION =====
+1. Celsius to Fahrenheit and Kelvin
+2. Fahrenheit to Celsius and Kelvin
+3. Kelvin to Celsius and Fahrenheit
+4. Exit
+==================================''')
+    try:
+        user = int(input('Choose operation (1-4): '))
+    except ValueError:
+        print('\nInvalid Input!\n')
+    else:
+        if user in [1,2,3]:
+            try:
+                temp = float(input('\nEnter temperature: '))
+            except ValueError:
+                print('\nInvalid Input!')
+            else:
+                if user == 1:
+                    if temp < -273.15:
+                        print('\nInvalid Input! Temperature can\'t be below -273.15°C.')
+                    else:
+                        print('\nTemperature Conversions:')
+                        print(f'{temp}°C = {(temp*9/5)+32}°F')
+                        print(f'{temp}°C = {temp + 273.15}K')
+                        if temp <= 0:
+                            print('\nIt\'s freezing! 🥶')
+                elif user == 2:
+                    if temp < -459.67:
+                        print('\nInvalid Input! Temperature can\'t be below -459.67°F.')
+                    else:
+                        print('\nTemperature Conversions:')
+                        print(f'{temp}°F = {round((temp-32)*5/9, 2)}°C')
+                        print(f'{temp}°F = {round((temp-32)*5/9, 2) + 273.15}K')
+                        if temp <= 32:
+                            print('\nIt\'s freezing! 🥶')
+                elif user == 3:
+                    if temp < 0:
+                        print('\nInvalid Input! Temperature can\'t be below 0K.')
+                    else:
+                        print('\nTemperature Conversions:')
+                        print(f'{temp}K = {round(temp - 273.15, 2)}°C')
+                        print(f'{temp}K = {round((temp-305.15)*5/9, 2)}°F')
+                        if temp <= 273.15:
+                            print('\nIt\'s freezing! 🥶')
+            finally:
+                print('\n==================================\n')
+        elif user == 4:
+            print('\nThanks for using Temperature Converter!')
+            break
+        else:
+            print('\nInvalid Input!\n')
